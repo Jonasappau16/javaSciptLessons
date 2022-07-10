4cc6ec8c3f48120f1a762428e3f1afd30fbfc73c
J@y14 <jonasappau16@gmail.com>
2022-07-10 12:30:26 -0400

    (Javascripts Operational Basic LEssons)

---
+++ index.js
@@ -0,0 +1,126 @@
+// alert("alert suck");
+// alert(alert suck)
+
+
+
+// BASIC SYNTAX & STRUCKER
+
+/* (NOTE FOR SNYTAX AND A GROUP LINE COMMENT)!!!
+ 1...javascript is case sensitive..
+2... always add your semicolon(;) to every statements in javascript
+ 3..Always use a new line for each single statement
+ 4. JavaSript runs statements from top to bottom
+ 5. Never start a variable with a number.
+ 6. camelcase is a must in javascript*/
+
+
+
+// alert("Hello World")
+
+     // VARIABLES (var)
+
+var myVariableShaun;
+myVariableShaun= 10;
+var myVar2 = "hello";
+myVar2="hello"
+
+
+/*we can always change the var as many times as we want to change it value ..
+we can also change the type as well (being a Number,string,boolean or whatever */
+
+myVar2 ="hello again"
+"hello again"
+myVar2=5;
+5
+
+var customerAge = 32;
+customerAge;32
+var myVar3 = 40;
+
+
+// Mathematical Operators 
+// The 5 most common signs { + % -  = *  }
+
+
+var myVar = 5
+undefined
+myVar 
+5
+
+// addition section 
+5+5
+10
+myVar + 10
+15
+myVar
+5
+// myVar = 5 from the above 
+myVar = myVar+ 10
+5+10 = 15
+10-5 = 5
+
+// subtraction section 
+myVar - 3
+// myVar we know from the above is assigned to the value 15 from line 56
+myVar - 3 ;
+15- 3 = 12
+
+myVar = myVar - 5
+15 - 5 =10
+
+// division section 
+myVar = myVar / 2 
+10/2 = 5
+
+   // String
+
+   5 + "hello" = 5hello 
+   
+   /*(it concastinate both values together to give an answer)*/
+    100 + hello = 100hello 
+    "hello" + "david"= "hellodavid"
+
+    5*hello = NaN /*( means you cant multiply a string by a numner )*/
+
+//     shorthand notation
+var myVar = 10
+myVar = myVar + 5 
+= 15
+
+myVar+ = 5 
+(15+5)
+20
+ myVar - = 10
+ 10
+
+ myVar / = 2
+ 5
+
+ myVar * = 5
+ 25
+
+ myVar + =1 
+ 26
+
+ myVar + + = myVar + 1
+ 27
+
+ ++myVar = 1 + 27 
+
+
+//  Logging to the console
+
+var myVar = "Shaun"
+
+document.write(myVar)
+// {document is an object and the text write/log is a writing}
+console.log(myVar)
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,6 @@
+        alert ("Hello world!");
+// (NOTE FOR SNYTEX)!!!
+// 1...javascript is case sensitive..
+//2... always add your semi-colon(;) to every statements in javascript
+// 3..A new line for each statement
+
